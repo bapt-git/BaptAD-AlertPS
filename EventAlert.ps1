107d7ac068f20eed51d024ef6d916e794ae5c900
bapt-git <93990351+bapt-git@users.noreply.github.com>
2021-11-18 16:14:24 +0100

    Create EventAlert.ps1
    PowerShell de base

---
+++ EventAlert.ps1
@@ -0,0 +1,66 @@
+param($eventID,$subjectusername,$targetusername,$targetsid,$subjectusersid)
+
+$Mailaddrrec = "bmaubey@simamoto.fr"
+$Mailaddrexp = "AD-Sima@simamoto.fr"
+$smtp = "smtp.simamoto.Fr"
+$logfil = "C:\Utilitaires\log\outputAD.txt"
+
+
+
+### Fonction : ctrl activation d'un utilisteur
+function event-activuser ($eventID,$subjectusername,$targetusername)
+{
+    "EventID: " + $eventID + ", " + $subjectusername + " a activé le compte utilisateur : " + $targetusername + ", a " + (get-date) | out-file $logfil -Append
+	Send-MailMessage -to $Mailaddrrec -subject 'Attention : activation de compte utilisateur AD !' -body 'Voir fichier en PJ' -smtpserver $smtp -from $Mailaddrexp -Attachments $logfil
+}
+
+### Fonction : ctrl désactivation d'un utilisateur
+function event-unactivuser ($eventID,$subjectusername,$targetusername)
+{
+    "EventID: " + $eventID + ", " + $subjectusername + " a desactive le compte utilisateur : " + $targetusername + ", a " + (get-date) | out-file $logfil -Append
+	Send-MailMessage -to $Mailaddrrec -subject 'Attention : desactivation de compte AD !' -body 'Voir fichier en PJ' -smtpserver $smtp -from $Mailaddrexp -Attachments $logfil
+}
+
+### Fonction : ctrl compte verrouillé
+function event-accountlocked ($eventID,$subjectusername,$targetusername)
+{
+    "EventID: " + $eventID + ", " + $subjectusername + " a verrouille le compte utilisateur : " + $targetusername + ", a " + (get-date) | out-file $logfil -Append
+	Send-MailMessage -to $Mailaddrrec -subject 'Attention : verrouillage de compte AD !' -body 'Voir fichier en PJ' -smtpserver $smtp -from $Mailaddrexp -Attachments $logfil
+}
+
+### Fonction ctrl mot de passe réinitialisé
+function event-pwdreset ($eventID,$subjectusername,$targetusername)
+{
+    "EventID: " + $eventID + ", " + $subjectusername + " a reinitialise le mot de passe utilisateur : " + $targetusername + ", a " + (get-date) | out-file $logfil -Append
+	Send-MailMessage -to $Mailaddrrec -subject 'Attention : reinitilisation de mot de passe utilisateur AD !' -body 'Voir fichier en PJ' -smtpserver $smtp -from $Mailaddrexp -Attachments $logfil
+}
+
+### Fonction ctrl compte supprimé
+function event-delaccount ($eventID,$subjectusername,$targetusername)
+{
+    "EventID: " + $eventID + ", " + $subjectusername + " a supprime le compte utilisateur : " + $targetusername + ", a " + (get-date) | out-file $logfil -Append
+	Send-MailMessage -to $Mailaddrrec -subject 'Attention : suppression de compte utilisateur AD !' -body 'Voir fichier en PJ' -smtpserver $smtp -from $Mailaddrexp -Attachments $logfil
+}
+
+
+### Appel des fonctions selon l'event ID MS
+if ($eventID -eq 4722)
+{
+    event-activuser -eventID $eventID -subjectusername $subjectusername -targetusername $targetusername
+}
+if ($eventID -eq 4740)
+{
+    event-accountlocked -eventID $eventID -subjectusername $subjectusername -targetusername $targetusername
+}
+if ($eventID -eq 4724)
+{
+    event-pwdreset -eventID $eventID -subjectusername $subjectusername -targetusername $targetusername
+}
+if ($eventID -eq 4726)
+{
+    event-delaccount -eventID $eventID -subjectusername $subjectusername -targetusername $targetusername
+}
+if ($eventID -eq 4725)
+{
+    event-unactivuser -eventID $eventID -subjectusername $subjectusername -targetusername $targetusername
+}
